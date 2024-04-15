28ff225308706dbbb665634e6229054adbc3b0b0
KatherineColon <katherinecolon@onmail.com>
2024-04-15 19:10:15 -0400

    added interest paid formula, renamed class

--- calculator1/src/main/java/com/pluralsight/FinancialCalculator.java
+++ calculator1/src/main/java/com/pluralsight/FinancialCalculator.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 import java.util.Scanner;
-public class calculator1 {
+public class FinancialCalculator {
  public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
 
@@ -14,8 +14,9 @@ public class calculator1 {
      int monthlyLoanTerm = yearlyLoanTerm *12;
      double monthlyInterestRate = annualInterestRate /100 /12;
      double monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, - monthlyLoanTerm));
+     double interestPaid = ((monthlyPayment * monthlyLoanTerm) - loanAmount);
 
-     System.out.printf("Your total monthly payment is: $%.2f", monthlyPayment);
-     System.out.printf("Your total interest over the life of the loan is: " );
+     System.out.printf("Your total monthly payment is: $%.2f \n", monthlyPayment);
+     System.out.printf("Your total interest paid: $%.2f", interestPaid);
     }
 }
